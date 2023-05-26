fda266eaf7c80ca43833fa504189ff879bf3bc26
Deltainu <109909833+Deltainu@users.noreply.github.com>
2023-05-26 20:13:00 +0200

    added Note Section in necessary Scripts and thoughts about structure

--- endpoints/game/GameConnection.js
+++ endpoints/game/GameConnection.js
@@ -1,3 +1,59 @@
+const GameService = require("./GameServices")
+
+let lobbys = {};
+
+// Arrow Function um die Funktionen nicht separat binden zu müssen
+const connection = (io) => {
+  io.on("connect", socket => {
+    //------- socket.on all handler -------//
+    socket.on("createGame", createGame);
+    //------- socket.on all handler -------//
+    socket.emit("connected", { message: "Connected successfully" });
+  });
+}
+
+function createGame() {
+
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  const codeCharacter = 9;
+  let code;
+  let checkedGameID = false;
+
+  do {
+    for (let i = 0; i < codeCharacter; i++) {
+      const randomCode = Math.floor(Math.random() * characters.length);
+      code += characters[randomCode];
+    }
+    if (!lobbys[code]) {
+      checkedGameID = true;
+    }
+
+  } while (!checkedGameID)
+
+
+
+
+}
+
+// Andere Exportierung um möglichen Plagiatsvorwurf aus vorlage SocketTriva zu vermeiden und konsistent zu bleiben (andere Module)
+module.exports = connection;
+
+
+
+//_________________________________________________Notes______________________________________________________________//
+/*
+Lobby als Hashmap nutzen. GameID ist der Key und der Wert kann wieder ein Objekt sein mit allen spielern und desen gemappten
+id und namen + punkte. locked um das spiel zu starten und keine Spieler mehr rein zu lassen über join.
+zb.
+ gameID key (propertie) = {player : [{socketid : [playername, points]}] , locked : boolean}
+*/
+
+
+
+
+
+//_________________________________________________OLD______________________________________________________________//
+/*
 const socketIO = require('socket.io')
 
 function generateCode(){
@@ -12,20 +68,22 @@ function generateCode(){
     return code;
 }
 
-// exports.createGame = (sio, socket) => {
-//     io = sio;
-//     gameSocket = socket;
-//     gaameSocket.on('joinLobby', joinLobby);
-// }
+exports.createGame = (sio, socket) => {
+    io = sio;
+    gameSocket = socket;
+    gaameSocket.on('joinLobby', joinLobby);
+}
 
 function joinLobby(lobbyId) {
     console.log('Lobby entering');
     // fetch token from API
     fetchToken().then((fetchedToken) => {
-      token = fetchedToken; 
+      token = fetchedToken;
       // method to add the client to the appropriate lobby room, based on the lobbyId passed in by the client
       gameSocket.join(`lobby-${lobbyId}`);
       // send token to client
       gameSocket.emit('token', token);
     });
   }
+*/
+//_________________________________________________OLD______________________________________________________________//
--- server.js
+++ server.js
@@ -60,7 +60,39 @@ routeNamespace.on('connect', socket => {
 });
 
 
+//_________________________________________________Notes______________________________________________________________//
+/*
 
-//_____________________________________________Prototyp Testing_______________________________________________________//
+- Eine Datei in der alle Handler stehen die für Sockets (routeNameSpace) benutzt wird. (GameConnection)
+- Eine Datei in der alle Abfragen an die Datenbank getätigt werden? (GameService)
+
+- GameConnection kann wie ein Konstruktor gebaut werden, dh. Es bekommt ein Socket Objekt und fügt diesem alles hinzu.
+zb. in server.js würde es so aussehen:
+
+const connection = require("./endpoints/game/GameConnection");
+const io = socketIO(server)
+const routeNamespace = io.of('/api/game'); 
+const VollständigerSocket = connection(routeNameSpace);
+
+oder kürzer
+
+const connection = require("./endpoints/game/GameConnection");
+const io = socketIO(server) 
+const VollständigerSocket = connection(io.of('/api/game'));
+
+damit wäre die Logik der handler in GameConnection ausgelagert.
+
+- In GameConnection werden dann die vorher definierten Handler hinzugefügt. Außerdem importet sich GameConnection das GameService Modul,
+welches von den Handler benutzt wird um zugriff auf die DB zu erhalten.
+
+-
+
+                                       
+- Cors bei socketIO möglicherweise überarbeiten wenn der Token gesendet werden soll.
+
+*/
+
+
+//_____________________________________________Prototyp Testing___________________________________________________//
 
 server.listen(80);
