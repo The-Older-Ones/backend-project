c0b41e11ff3495022b9cb48071d02fa70c859114
s86197 <vioannidou3@gmail.com>
2023-05-30 21:23:01 +0200

    getRandomQuestion_function_in_gameService

--- endpoints/game/GameServices.js
+++ endpoints/game/GameServices.js
@@ -1,4 +1,5 @@
 const CategoryList = require ("../../database/TriviaQuestions/CategoryListModel");
+const Question = require("../../database/TriviaQuestions/QuestionModel")
 
 async function getCategoryList() {
     try {
@@ -9,11 +10,32 @@ async function getCategoryList() {
     }
   }
 
-module.exports = {
-    getCategoryList
+async function getRandomQuestion(categoryQuestion, difficultyQuestion) {
+  try {
+    const questions = await Question.find({ category: categoryQuestion, difficulty: difficultyQuestion });
+    
+    if (questions.length === 0) {
+      return { error: 'No questions found for the given category and difficulty.' };
+    }
+
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    const randomQuestion = questions[randomIndex];
+
+    const questionObject = {
+      "category": randomQuestion.category,
+      "difficulty": randomQuestion.difficulty,
+      "question": randomQuestion.question,
+      "correct_answer": randomQuestion.correct_answer,
+      "incorrect_answers": randomQuestion.incorrect_answers
+    };
+
+    return questionObject;
+  } catch (error) {
+    return { error: 'An error occurred while retrieving the question.' };
+  }
 }
 
-//_________________________________________________Notes______________________________________________________________//
-/*
-- Funktion für Random Frage. Parameter category und difficulty. Rückgabe Objekt
-*/
+module.exports = {
+    getCategoryList,
+    getRandomQuestion
+}
--- test.js
+++ test.js
@@ -1,10 +1,35 @@
-const io = require('socket.io-client');
+// const io = require('socket.io-client');
 
-const socket = io ('http://127.0.0.1:80');
+// const socket = io ('http://127.0.0.1:80');
 
-socket.io('connect', () => {
-    console.log('Connected to server');
-})
+// socket.io('connect', () => {
+//     console.log('Connected to server');
+// })
 
 
-// TODO - to test socket.io 
+// // TODO - to test socket.io 
+const gameServices = require ("./endpoints/game/GameServices")
+const database = require('./database/db');
+
+database((err) => {
+    if (err) {
+        console.log("Error: Could not connect to database " + err.message);
+    } else {
+        console.log("Successful: connection to the database established");
+        questionInit();
+    }
+});
+
+async function testGetRandomQuestion() {
+    // Call getRandomQuestion with sample category and difficulty
+    const result = await gameServices.getRandomQuestion("gaga", "200");
+  
+    // Check the result
+    if ('error' in result) {
+      console.error(result.error);
+    } else {
+      console.log('Random Question:', result);
+    }
+  }
+
+  testGetRandomQuestion();
