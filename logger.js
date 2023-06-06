0beff05c20becb09702cda6cb9dd876b58f2d7e2
Deltainu <109909833+Deltainu@users.noreply.github.com>
2023-06-07 00:33:54 +0200

    update logger. Now its possible dto deactivate logger in config when all logginEnable are false

--- logger.js
+++ logger.js
@@ -1,20 +1,34 @@
 const winston = require("winston");
 const config = require("config");
-const path = require("path");
 
-const logger = winston.createLogger({
+let logger;
+
+if (!config.loggingEnabledFile && !config.loggingEnabledConsole) {
+  logger = {
+    error: () => { },
+    warn: () => { },
+    warning: () => { },
+    info: () => { },
+    http: () => { },
+    verbose: () => { },
+    debug: () => { },
+    silly: () => { }
+  }
+} else {
+  logger = winston.createLogger({
     level: config.loggingLevel,
     format: winston.format.combine(
       winston.format.timestamp({ format: "DD-MM-YYYY HH:mm:ss" }),
       winston.format.printf(info => {
-        const { timestamp, level, message} = info;
+        const { timestamp, level, message } = info;
         return `${timestamp} [${level}] [${message}]`;
       })
     ),
     transports: []
   });
+};
 
-if (config.loggingEnabled) {
+if (config.loggingEnabledFile) {
   logger.add(
     new winston.transports.File({
       filename: "logs/app.log",
@@ -24,8 +38,9 @@ if (config.loggingEnabled) {
     })
   );
 }
-if(config.loggingEnabledConsole){
+
+if (config.loggingEnabledConsole) {
   logger.add(new winston.transports.Console());
 }
 
-  module.exports = logger;
+module.exports = logger;
